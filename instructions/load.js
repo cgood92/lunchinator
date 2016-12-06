17caf01198bf00e47a8e0fdd4dcf2e1acd620236
Clint Goodman <goodmancf@ldschurch.org>
2016-12-05 18:06:43 -0700

    return promises for loading, so that we know when it’s done

--- instructions/load.js
+++ instructions/load.js
@@ -6,46 +6,52 @@ const bcrypt = require('bcrypt'),
 
 
 const loadUsers = () => {
-	const csv = new CSVConverter({
-		headers: ['username','password','scope']
-	});
-	csv.fromFile('./instructions/users.csv', (err,result) => {
-		Promise.all(result.map((user) => {
-			return new Promise((resolve, reject) => {
-				// Hash the password
-				bcrypt.hash(user.password, 10 /* SALT */, (err, hash) => {
-					db.insert('users').data({
-						username: user.username,
-						password: hash,
-						scope: user.scope
-					}).then(() => {
-						resolve();
+	return new Promise((resolve, reject) => {
+		const csv = new CSVConverter({
+			headers: ['username','password','scope']
+		});
+		csv.fromFile('./instructions/users.csv', (err,result) => {
+			Promise.all(result.map((user) => {
+				return new Promise((resolve, reject) => {
+					// Hash the password
+					bcrypt.hash(user.password, 10 /* SALT */, (err, hash) => {
+						db.insert('users').data({
+							username: user.username,
+							password: hash,
+							scope: user.scope
+						}).then(() => {
+							resolve();
+						});
 					});
 				});
+			})).then(() => {
+				console.log('Users have been loaded');
+				resolve();
 			});
-		})).then(() => {
-			console.log('Users have been loaded');
 		});
 	});
 };
 
 const loadRestaurants = () => {
-	const csv = new CSVConverter({
-		headers: ['id','name']
-	});
-	csv.fromFile('./instructions/restaurants.csv', (err,result) => {
-		Promise.all(result.map((restaurant) => {
-			return new Promise((resolve, reject) => {
-				db.insert('restaurants').data({
-					name: restaurant.name,
-					isSlow: restaurant.isSlow || false,
-					daysAgo: restaurant.daysAgo || 0
-				}).then(() => {
-					resolve();
+	return new Promise((resolve, reject) => {
+		const csv = new CSVConverter({
+			headers: ['id','name']
+		});
+		csv.fromFile('./instructions/restaurants.csv', (err,result) => {
+			Promise.all(result.map((restaurant) => {
+				return new Promise((resolve, reject) => {
+					db.insert('restaurants').data({
+						name: restaurant.name,
+						isSlow: restaurant.isSlow || false,
+						daysAgo: restaurant.daysAgo || 0
+					}).then(() => {
+						resolve();
+					});
 				});
+			})).then(() => {
+				console.log('Restaurants have been loaded');
+				resolve();
 			});
-		})).then(() => {
-			console.log('Restaurants have been loaded');
 		});
 	});
 };
